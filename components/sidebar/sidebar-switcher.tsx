3ac5a97674a54ea1400368d27518d370d34a1668
Brad <bmpalagi@gmail.com>
2025-05-13 22:25:09 -0500

    stop causing havoc. small changes

--- components/sidebar/sidebar-switcher.tsx
+++ components/sidebar/sidebar-switcher.tsx
@@ -33,21 +33,39 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
           onContentTypeChange={onContentTypeChange}
         />
 
-        <SidebarSwitchItem
+        {/* <SidebarSwitchItem
           icon={<IconAdjustmentsHorizontal size={SIDEBAR_ICON_SIZE} />}
           contentType="presets"
           onContentTypeChange={onContentTypeChange}
-        />
+        /> */}
 
         <SidebarSwitchItem
           icon={<IconPencil size={SIDEBAR_ICON_SIZE} />}
-          contentType="prompts"
+          contentType="workflows"
           onContentTypeChange={onContentTypeChange}
         />
 
         <SidebarSwitchItem
+          icon={<IconBolt size={SIDEBAR_ICON_SIZE} />}
+          contentType="actions"
+          onContentTypeChange={onContentTypeChange}
+        />
+
+        {/* <SidebarSwitchItem
+          icon={<IconPencil size={SIDEBAR_ICON_SIZE} />}
+          contentType="prompts"
+          onContentTypeChange={onContentTypeChange}
+        /> */}
+
+        {/* <SidebarSwitchItem
           icon={<IconSparkles size={SIDEBAR_ICON_SIZE} />}
           contentType="models"
+          onContentTypeChange={onContentTypeChange}
+        /> */}
+
+        <SidebarSwitchItem
+          icon={<IconBooks size={SIDEBAR_ICON_SIZE} />}
+          contentType="collections"
           onContentTypeChange={onContentTypeChange}
         />
 
@@ -57,23 +75,17 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
           onContentTypeChange={onContentTypeChange}
         />
 
-        <SidebarSwitchItem
-          icon={<IconBooks size={SIDEBAR_ICON_SIZE} />}
-          contentType="collections"
-          onContentTypeChange={onContentTypeChange}
-        />
-
-        <SidebarSwitchItem
+        {/* <SidebarSwitchItem
           icon={<IconRobotFace size={SIDEBAR_ICON_SIZE} />}
           contentType="assistants"
           onContentTypeChange={onContentTypeChange}
-        />
+        /> */}
 
-        <SidebarSwitchItem
+        {/* <SidebarSwitchItem
           icon={<IconBolt size={SIDEBAR_ICON_SIZE} />}
           contentType="tools"
           onContentTypeChange={onContentTypeChange}
-        />
+        /> */}
       </TabsList>
 
       <div className="flex flex-col items-center space-y-4">
